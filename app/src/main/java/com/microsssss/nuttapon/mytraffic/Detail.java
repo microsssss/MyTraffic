ad31169bfff12fced328a32f642823345b982473
nuttaponk <microsssss@gmail.com>
2017-02-05 15:13:19 +0700

    End not ok

--- app/src/main/java/com/microsssss/nuttapon/mytraffic/Detail.java
+++ app/src/main/java/com/microsssss/nuttapon/mytraffic/Detail.java
@@ -2,6 +2,9 @@ package com.microsssss.nuttapon.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class Detail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        TextView titleTextView = (TextView) findViewById(R.id.textView4);
+        titleTextView.setText(getIntent().getStringExtra("Title"));
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView.setImageResource(getIntent().getIntExtra("Image", R.drawable.traffic_01));
+
+        TextView detailTextview=(TextView) findViewById(R.id.textView4);
+        detailTextview.setText(getIntent().getStringExtra("Detail"));
+
+    }  // Main Method
+
+    public void clickback(View view) {    // ต้องเข้าไปเพิ่ม android:onClick="clickback" ใน Activity
+        finish();
     }
-}
+
+
+
+
+}  //Main Class
